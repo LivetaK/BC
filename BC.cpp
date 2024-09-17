239a22bc19e5b9703235b0f4e03da24e691289ba
Liveta <livetakaval@gmail.com>
2024-09-17 22:09:11 +0300

    Add project files.

---
+++ BC.cpp
@@ -0,0 +1,11 @@
+#include "Header.hpp"
+//0123456789abcdef
+using namespace std;
+
+int main() {
+	string input;
+	cin >> input;
+	int length = input.length();
+	code(input, length);
+	
+}
---
+++ Header.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "string.h"
+#include <vector>
+string code(string input, int length) {
+	if (length > 64) {
+
+	}
+}
