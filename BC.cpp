dd2aebe4acd329d25ebc23a5c4c17adea93a11b9
Liveta <livetakaval@gmail.com>
2024-09-18 13:32:19 +0300

    -pridetas vektorius

--- BC.cpp
+++ BC.cpp
@@ -9,8 +9,10 @@ int main() {
 	cin >> input;
 	vector<char> inputVector(input.begin(), input.end());
 	int sandauga = 1;
-	vector<char> code;
+	vector<int> code;
+	int i = 0;
 	for (char c : inputVector) {
+		i++;
 		int a = convert(c);
 	    cout << a <<endl;
 		sandauga *= a;
@@ -28,8 +30,15 @@ int main() {
 			cout << endl;
 			sandauga = 1;
 		}
-	}
+		if (sandauga != 1 && i == inputVector.size()) {
+			code.push_back(sandauga);
+		}
 
+	}
+	for (int c : code) {
+		cout << "labas" << endl;
+		cout << c << endl;
+	}
 	//int length = input.length();
 	//code(input, length);
 	//int a = convert(input);
